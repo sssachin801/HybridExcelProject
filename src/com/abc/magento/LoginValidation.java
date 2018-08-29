0a10e9c131754e2dd8ad07e9824dfc2781ae1098
sssachin801 <sssachin801@gmail.com>
2018-08-29 13:31:32 +0530

    first commit

---
+++ src/com/abc/magento/LoginValidation.java
@@ -0,0 +1,12 @@
+package com.abc.magento;
+
+import java.io.FileInputStream;
+
+public class LoginValidation 
+{
+	public static void main(String args[])throws Exception
+	{
+		FileInputStream fis=new FileInputStream("//so");
+	}
+
+}
